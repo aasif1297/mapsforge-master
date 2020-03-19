c5a2e8f0acc67201e5bcf75859a5ffb833a725c8
Asif Jawaid <asif.jawaid@celeritas-solutions.com>
2020-03-19 18:30:33 +0500

    Initial commit

---
+++ src/main/java/org/mapsforge/samples/android/MyActivity.java
@@ -0,0 +1,15 @@
+package org.mapsforge.samples.android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+}
